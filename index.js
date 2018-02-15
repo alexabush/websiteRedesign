f9431927236dcccab1695426c8dff9373d1a65e0
Alex Bush <alexabush7@gmail.com>
2018-02-14 17:38:27 -0800

    replaced tree img with gear img for project and added anchors to nav bar links

--- index.js
+++ index.js
@@ -4,12 +4,8 @@ $(document).ready(function() {
   const $nav = $('nav')
 
   for (let i = $imgs.length - 2; i < $imgs.length; i++) {
-    $($imgs[i]).attr('src', './img/trees.jpg');
+    $($imgs[i]).attr('src', './img/gear_art.jpg');
   }
-
-  // $.each($imgs, function(index, value) {
-  //   $(value).attr('src', './img/trees.jpg');
-  // });
 
   $(window).scroll(function() {
     var scroll = $(window).scrollTop();
